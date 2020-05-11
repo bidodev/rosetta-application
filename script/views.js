557e87617c44735ad6f404c34cdcf26c9f60e0e9
Claudinei Bido <bido.c@live.com>
2020-05-11 22:07:12 +0200

    Documentation Google API calls examples Goold API queries examples and filters Code preparation

--- script/views.js
+++ script/views.js
@@ -8,21 +8,25 @@ export const clearResults = () => {
 };
 
 const renderBook = book => {
-  let { title, description, authors, imageLinks } = book.volumeInfo;
-
-  description ? description : (description = "No Description");
+  let { title, description, authors, pageCount, imageLinks } = book.volumeInfo;
 
   let markUp = `
     <div class="description">
     <div class="title">Title: ${title}</div>
-    <div class="sub-desc">Authors: ${authors}</div>
+    <div class="sub-desc">Authors: <a>${renderBooksbyAuthor(
+      authors
+    )}</a> . Pages: ${pageCount}</div>
     <figure class="thumb-image">
       <img src="${imageLinks.thumbnail}" alt="${title}" srcset="">
     </figure>
-    <div class="book-desc">${description}</div>
+    <div class="book-desc">${description} <a href="http://">Read More...</a></div>
   </div>`;
 
   elements.resultDiv.insertAdjacentHTML("beforeend", markUp);
+};
+
+const renderBooksbyAuthor = authors => {
+  return `${authors}`;
 };
 
 export const renderResults = data => {
