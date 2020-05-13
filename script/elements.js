467fa462964cf363ba5c94eca29914d0f9184397
melissa <nadia.mm.mariduena@gmail.com>
2020-05-13 11:55:42 +0200

    files deleted

--- script/elements.js
+++
@@ -1,6 +0,0 @@
-export const DOMstrings = {
-  searchType: document.querySelector(".search-type"),
-  resultDiv: document.querySelector(".result"),
-  fetchBtn: document.querySelector(".fetch-values"),
-  searchQuery: document.querySelector(".search-field"),
-};
