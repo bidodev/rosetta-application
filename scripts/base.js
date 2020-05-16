d646c596b350f1e7209d2589c5c605113bc370a1
Claudinei Bido <bido.c@live.com>
2020-05-16 11:54:41 +0200

    Updates

--- scripts/base.js
+++ scripts/base.js
@@ -20,6 +20,7 @@ export const elements = {
   navbar: document.querySelector(".header-nav"),
   pagination: document.querySelector(".pagination"),
   main: document.querySelector(".main-content"),
+  spinnerArea: document.querySelector(".spinnerArea"),
 };
 
 export const configs = {
@@ -35,11 +36,7 @@ export const elementsStr = {
 //insert the spinner loader..
 export const renderSpinner = (parent, where) => {
   const loader = `
-  <div class="${elementsStr.loader}">
-    <svg>
-      <use href="img/icons.svg#icon-cw"></use>
-    </svg>
-  </div>`;
+  <div class="${elementsStr.loader}"></div>`;
   parent.insertAdjacentHTML(where, loader);
   /** element.insertAdjacentHTML(position, text);
    * Parameters
